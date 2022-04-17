7fa8e0f226bc03bc5802b9461b0f76e89df33c85
MihailTsybakov <62279777+MihailTsybakov@users.noreply.github.com>
2022-04-17 13:34:06 +0300

    Memory efficiency increased

--- Sparse_Visualizer.py
+++ Sparse_Visualizer.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from scipy.sparse import csr_array
 
 def print_matrix(matrix : list):
     for row in matrix:
@@ -37,11 +38,66 @@ def read_CSR(filename: str):
         matrix.append(tmp_row)
     
     return matrix
-    
-    
-matrix_1 = 'Sparse_Matrix_1.txt'
 
-Matrix = read_CSR('Sparse_Matrix_3.txt')
+def read_CSR_bool(filename : str):
+    matrix = []
+    
+    input_file = open(filename, 'r')
+    txt_data = input_file.read()
+    
+    parse_tokens = txt_data.split('\n')
+    
+    N_NNZ = (parse_tokens[0]).split(' ')
+    
+    N = int(N_NNZ[0])
+    NonZero = int(N_NNZ[1])
+    
+    rows = list(map(lambda elem: int(elem), parse_tokens[1 : N + 2] ))
+    cols = list(map(lambda elem: int(elem), parse_tokens[N + 2 : N + 2 + NonZero]))
+    elements_bool = list(map(lambda elem: True if float(elem) != 0 else False, parse_tokens[N + 2 + NonZero : N + 3 + 2*NonZero]))
+    
+    elems_read = 0
+    
+    for r in range(N):
+        
+        tmp_row = [False for i in range(N)]
+        elems_in_row = rows[r + 1] - rows[r]
+        
+        columns = cols[ elems_read : elems_read + elems_in_row ]
+        
+        for i in range(len(columns)):
+            tmp_row[columns[i]] = elements_bool[elems_read + i]
+            
+        elems_read += elems_in_row
+        matrix.append(tmp_row)
+    
+    input_file.close()
+    return matrix    
+    
 
-#Plotting
-plt.spy(Matrix)
+def read_CSR_components(filename : str):
+    matrix = []
+    
+    input_file = open(filename, 'r')
+    txt_data = input_file.read()
+    
+    parse_tokens = txt_data.split('\n')
+    
+    N_NNZ = (parse_tokens[0]).split(' ')
+    
+    N = int(N_NNZ[0])
+    NonZero = int(N_NNZ[1])
+    
+    rows = list(map(lambda elem: int(elem), parse_tokens[1 : N + 2] ))
+    cols = list(map(lambda elem: int(elem), parse_tokens[N + 2 : N + 2 + NonZero]))
+    elements = list(map(lambda elem: float(elem), parse_tokens[N + 2 + NonZero : N + 3 + 2*NonZero]))
+    
+    input_file.close()
+    return (elements, rows, cols, N)
+       
+
+data, row_ind, col_ind, N = read_CSR_components('Sparse_Matrix_1.txt')
+
+scipy_CSR = csr_array((data, col_ind, row_ind), shape = (N,N))
+plt.spy(scipy_CSR, markersize = 0.01)
+plt.savefig('Matrix.png')
